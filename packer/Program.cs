3ddfbe863b36383740814a4c4f1034293795190e
TheClashFruit <clash.fruit123@gmail.com>
2022-12-27 13:45:47 +0100

    feat: stuff

--- packer/Program.cs
+++ packer/Program.cs
@@ -2,26 +2,24 @@
 {
     static void Main(string[] args)
     {
-        switch (args[0])
+        for (var i = 0; i < args.Length; i++)
         {
-            case "--license":
-                Console.WriteLine("Please visit https://github.com/TheClashFruit/packer/blob/main/LICENSE for the full license.\n");
-                
-                Console.WriteLine("  packer  Copyright (C) 2022  TheClashFruit");
-                Console.WriteLine("  This program comes with ABSOLUTELY NO WARRANTY; for details type `packer --license'.");
-                Console.WriteLine("  This is free software, and you are welcome to redistribute it");
-                Console.WriteLine("  under certain conditions; type `packer --license' for details.");
-                break;
-            case "--new":
-                if (args[1] != "")
-                {
-                    Console.WriteLine("Creating " + args[1] + ".");
-                }
-                
-                break;
-            default:
-                Console.WriteLine("help");
-                break;
+            switch (args[i])
+            {
+                case "--license":
+                    Console.WriteLine("packer Copyright (C) 2022 TheClashFruit");
+                    Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY; for details type `packer --fl'.");
+                    Console.WriteLine("This is free software, and you are welcome to redistribute it");
+                    Console.WriteLine("Under certain conditions; type `packer --fl' for details.");
+                    break;
+                case "--new":
+                    if (!String.IsNullOrEmpty(args[i + 1]))
+                    {
+                        Console.WriteLine("Creating...");
+                        Console.WriteLine("Created " + args[i + 1]);   
+                    }
+                    break;
+            }
         }
     }
 }
